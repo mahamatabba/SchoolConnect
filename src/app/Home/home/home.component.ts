1458ae1fb571d574385b9c2bdd91198d9195184f
Izzou@6398 <izzedinemabba@gmail.com>
2024-07-27 17:44:13 +0200

    page d'acceuil

--- src/app/Home/home/home.component.ts
+++ src/app/Home/home/home.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  public canInstall: boolean = false;
+  private deferredPrompt: any;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: Event): void {
+    event.preventDefault();
+    this.deferredPrompt = event;
+    this.canInstall = true;
+  }
+
+  installPwa(): void {
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Utilisateur accepte l\'installation');
+      } else {
+        console.log('Utilisateur refuse l\'installation');
+      }
+      this.deferredPrompt = null;
+      this.canInstall = false;
+    });
+  }
+
 
 }
